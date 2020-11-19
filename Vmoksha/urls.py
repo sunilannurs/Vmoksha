9fa124086e1a21bac4f2087a6e3709848911f1c0
sunil <sunils051194@gmail.com>
2020-11-19 20:17:16 +0530

    changes

--- Vmoksha/urls.py
+++ Vmoksha/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('preference/', include('preference.urls')),
+    path('', include('preference.urls')),
 ]
